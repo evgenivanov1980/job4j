12e97640d2dd7e0490fa731e2b963843efdc0ca1
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2019-03-17 19:25:53 +0500

    Максимум из трех

--- chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MultiMaxTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertThat;
 
 public class MultiMaxTest {
     @Test
-    public void WhenSecondMax() {
+    public void whenSecondMax() {
     MultiMax check = new MultiMax();
     int result  = check.max(1, 4, 2);
     assertThat(result, is(4));
