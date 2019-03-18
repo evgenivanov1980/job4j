71c0c34b962290af5dedeba0d035e5044af7c83c
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2019-03-18 09:38:04 +0500

    Максимум из трех

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -1,6 +1,13 @@
 package ru.job4j.condition;
 
 public class MultiMax {
+    /**
+     * Метод вычисления максимального числа из трех
+     * @param first первое число
+     * @param second второе число
+     * @param third третье число
+     * @return возврашет максимальное число из трех
+     */
     public int max(int first, int second, int third) {
         int result = first > second ? first : second;
         return result > third ? result : third;
