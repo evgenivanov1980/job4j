76294554ade1f4ffb72d6a43882e632a527301e6
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2019-03-19 22:22:24 +0500

    Построение шахматной доски в псевдографике

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -4,18 +4,20 @@ public class Board {
     public String paint(int width, int weight) {
         StringBuilder screen = new StringBuilder();
         String ln = System.lineSeparator();
-        for (int i = 0; i <= width; i++) {
-            for (int j = 0; j <= weight; j++) {
-                if (i + j % 2 == 0) {
+        for (int i = 0; i < weight; i++) {
+            for (int j = 0; j < width; j++) {
+                if ((i + j) % 2 == 0) {
                     screen.append("X");
                 } else {
-                    screen.append("0");
+                    screen.append(" ");
                 }
             }
+            screen.append(ln);
         }
-        screen.append(ln);
-        return toString();
+        return screen.toString();
 
 
     }
+
+
 }
--- chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -15,6 +15,25 @@ import static org.junit.Assert.assertThat;
 
 public class BoardTest {
     @Test
-    public void when3x3 {
+    public void when3x3() {
+        Board board = new Board();
+        String rs1 = board.paint(3, 3);
+        String ln = System.lineSeparator();
+        assertThat(rs1, is(
+                String.format("X X%s X %sX X%s", ln, ln, ln)));
 
+    }
+
+
+
+
+    @Test
+    public void when5x4() {
+        Board board = new Board();
+        String rs2 = board.paint(5, 4);
+        String ln = System.lineSeparator();
+        assertThat(rs2, is(
+                String.format("X X X%s X X %sX X X%s X X %s", ln, ln, ln, ln)));
+
+    }
 }
