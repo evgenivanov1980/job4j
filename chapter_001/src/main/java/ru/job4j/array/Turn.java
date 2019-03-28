90ea2d14e0a6b6159037741223453aa627e2ff65
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2019-03-28 13:20:18 +0500

    Переворот массива

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -3,16 +3,15 @@ package ru.job4j.array;
 public class Turn {
     public int[] back(int[] array) {
         int tmp = 0;
-        for (int index = 0; index < array.length; index++) {
+        for (int index = 0; index < array.length / 2; index++) {
             tmp = array[index];
             array[index] = array[array.length - index - 1];
             array[array.length - index - 1] = tmp;
-            if ((array.length / 2 - 1 == index) || array.length / 2 == index) {
-                break;
-            }
-
-
         }
+
+
+
+
         return array;
     }
 
