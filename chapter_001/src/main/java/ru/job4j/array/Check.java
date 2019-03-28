5fc2325351fd4f856a0482c1f8ec5b61c55bee8f
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2019-03-28 13:17:29 +0500

    Массив заполнен true or false?

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+        import jdk.nashorn.internal.ir.WhileNode;
+
+public class Check {
+    public boolean mono(boolean[] date) {
+        boolean result = true;
+        for (int index = 0; index < date.length; index++) {
+            if (result != date[index]) {
+                result = false;
+
+            }
+
+
+        }
+        return result;
+
+
+    }
+}
