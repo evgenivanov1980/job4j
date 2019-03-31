33b7cc54b4d7f424fe72e5d1accaac4859d1bcd0
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2019-03-31 21:34:07 +0500

    Квадратичный массив заполнен true or false

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+
+public class MatrixCheck {
+
+    /**
+     * Метод проверяет в квадратичном массиве диагонали на заполнение значениями true or false
+     * @param date квадратичный массив
+     * @return возвращет true если все логические значения по большим диагоналям равны, и false если не равны
+     */
+
+    public boolean mono(boolean[][] date) {
+        boolean result = true;
+        for (int i = 0; i < date.length; i++) {
+            if (date[i][i] != date[0][date.length - 1] || date[i][i] != date[date.length - 1][0]) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
