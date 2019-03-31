89fa0775474cb2be7d796b8d8617112391b47c86
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2019-03-31 21:35:23 +0500

    Алгоритм пузырьковой сортировки

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -3,12 +3,12 @@ package ru.job4j.array;
 public class BubbleSort {
     public int[] sort(int[] array) {
         int tmp = 0;
-        for (int i = 1; i <= array.length; i++) {
-            for (int j = array.length - 1; j >= i; j--) {
-                if (array[j - 1] > array[j]) {
+        for (int i = array.length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
                     tmp = array[j];
-                    array[j] = array[j - 1];
-                    array[j - 1] = tmp;
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
                 }
             }
 
