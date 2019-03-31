8092d0a8302719ae21758a53ea89f897321326b8
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2019-03-31 22:14:51 +0500

    Проверка диагоналей квадратичного массива на равенство логических значений.

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -12,7 +12,7 @@ public class MatrixCheck {
     public boolean mono(boolean[][] date) {
         boolean result = true;
         for (int i = 0; i < date.length; i++) {
-            if (date[i][i] != date[0][date.length - 1] || date[i][i] != date[date.length - 1][0]) {
+            if (date[i][i] != date[0][0] && date[i][date.length - 1] != date[i][date.length - i - 1])  {
                 result = false;
             }
         }
