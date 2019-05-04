570ed651b60caadc592c5273e26777711adf3410
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2019-05-04 17:11:31 +0500

    Реализация класса StartUI

--- chapter_002/src/main/java/ru/job4j/start/StartUITest.java
+++ chapter_002/src/main/java/ru/job4j/start/StartUITest.java
@@ -2,7 +2,7 @@ package ru.job4j.start;
 
 public class StartUITest {
     public static void main(String[] args) {
-        Input input = (new StubInput(new String[]{"create stub task"}));
+        Input input = (new StubInput(new String[]{"0", "testname", "testdesk", "6"}));
         Tracker tracker = new Tracker();
         new StartUI(input, tracker).init();
 
