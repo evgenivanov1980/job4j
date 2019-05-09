f64f809af8aabe9d852036d2f44a3792f81dc6f2
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2019-05-09 16:33:34 +0500

    Changed into pom.xml

--- chapter_002/src/main/java/ru/job4j/start/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/start/StartUI.java
@@ -149,7 +149,9 @@ public class StartUI {
         Item item = new Item(changedname, changeddescription);
         if (this.tracker.replace(id, item)) {
             System.out.println("------ Новая заявка с id:" + item.getId() + "------");
-        } else System.out.println("------ Заявка не найдена -----");
+        } else {
+            System.out.println("------ Заявка не найдена -----");
+        }
 
 
     }
@@ -181,37 +183,35 @@ public class StartUI {
         System.out.println("---- Заявка с таким id не найдена -----");
     }
 
-        /**
-         * Метод поиска заявки по имени
-         */
-        public void findItemByName () {
-            System.out.println("----Поиск заявки по имени----");
-            String name = this.input.ask("Введите имя заявки");
-            Item[] item = this.tracker.findByName(name);
-            System.out.println("Найденная заявка" + Arrays.toString(item) + "---");
-
-        }
-
-
-        private void showMenu () {
-            System.out.println("Меню");
-            System.out.println("Добавить новую заявку");
-            System.out.println("Показать все заявки");
-            System.out.println("Редактировать зявку");
-            System.out.println("Удалить заявку");
-            System.out.println("Найти заявку по id");
-            System.out.println("Найти заявку по имени");
-            System.out.println("Выход из программы");
-        }
-
-        /**
-         * Запуск программы
-         *
-         * @param args
-         */
-
-        public static void main (String[]args){
-            new StartUI(new ConsoleInput(), new Tracker()).init();
-        }
+    /**
+     * Метод поиска заявки по имени
+     */
+    public void findItemByName() {
+        System.out.println("----Поиск заявки по имени----");
+        String name = this.input.ask("Введите имя заявки");
+        Item[] item = this.tracker.findByName(name);
+        System.out.println("Найденная заявка" + Arrays.toString(item) + "---");
 
     }
+
+    private void showMenu() {
+        System.out.println("Меню");
+        System.out.println("Добавить новую заявку");
+        System.out.println("Показать все заявки");
+        System.out.println("Редактировать зявку");
+        System.out.println("Удалить заявку");
+        System.out.println("Найти заявку по id");
+        System.out.println("Найти заявку по имени");
+        System.out.println("Выход из программы");
+    }
+
+    /**
+     * Запуск программы
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        new StartUI(new ConsoleInput(), new Tracker()).init();
+    }
+
+}
