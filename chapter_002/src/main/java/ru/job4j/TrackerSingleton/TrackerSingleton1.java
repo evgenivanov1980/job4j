92279682e74654f032eed75d4df6e26b0ea5515f
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2019-05-26 14:54:47 +0500

    Singleton Tracker

---
+++ chapter_002/src/main/java/ru/job4j/TrackerSingleton/TrackerSingleton1.java
@@ -0,0 +1,31 @@
+package ru.job4j.TrackerSingleton;
+
+import ru.job4j.models.Item;
+
+public enum  TrackerSingleton1 {
+    INSTANCE;
+
+
+
+    public Item add(Item item) {
+        return item;
+    }
+
+    public boolean replace(String id, Item item) {
+        return replace(id, item);
+    }
+
+    public boolean delete(String id) {
+        return delete(id);
+    }
+
+    public Item[] findall()
+
+    public static void main(String[] args) {
+        TrackerSingleton1 tracker = TrackerSingleton1.INSTANCE;
+
+
+    }
+
+
+}
