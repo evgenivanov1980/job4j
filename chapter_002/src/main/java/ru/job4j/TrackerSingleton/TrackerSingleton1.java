3740fa37a2a9da25c15293e0e480df3e6c727c97
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2019-05-26 17:08:18 +0500

    Singleton Tracker

--- chapter_002/src/main/java/ru/job4j/TrackerSingleton/TrackerSingleton1.java
+++ chapter_002/src/main/java/ru/job4j/TrackerSingleton/TrackerSingleton1.java
@@ -1,9 +1,15 @@
 package ru.job4j.TrackerSingleton;
 
+
+
 import ru.job4j.models.Item;
+
+import java.util.Arrays;
 
 public enum  TrackerSingleton1 {
     INSTANCE;
+
+
 
 
 
@@ -19,7 +25,9 @@ public enum  TrackerSingleton1 {
         return delete(id);
     }
 
-    public Item[] findall()
+    public Item[] findall() {
+        return findall();
+    }
 
     public static void main(String[] args) {
         TrackerSingleton1 tracker = TrackerSingleton1.INSTANCE;
---
+++ chapter_002/src/main/java/ru/job4j/TrackerSingleton/TrackerSingleton2.java
@@ -0,0 +1,9 @@
+package ru.job4j.TrackerSingleton;
+
+import ru.job4j.start.Tracker;
+
+public class TrackerSingleton2 {
+
+    private static TrackerSingleton2 instance;
+
+}
