5cd895cff4254feb7ecc2a2724deb428b7b13a96
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2019-06-19 15:41:16 +0500

    Coffeemashine

--- chapter_002/src/main/java/Coffeemashine.java
+++ chapter_002/src/main/java/Coffeemashine.java
@@ -6,13 +6,13 @@ public class Coffeemashine {
 
     public int[] changes(int value, int price) {
         int index = 0;
-        int oddmaney = value - price;
+        int oddmoney = value - price;
         int[] coins = new int[] {1, 2, 5 ,10};
         result = new int[coins.length];
         for (int i = 0; i < coins.length; i++) {
-            if (oddmaney >= coins[coins.length - i -1]) {
+            if (oddmoney >= coins[coins.length - i -1]) {
                 result[index] = coins[coins.length - i -1];
-                oddmaney = oddmaney % coins[coins.length - i -1];
+                oddmoney = oddmoney % coins[coins.length - i -1];
                 index++;
 
 
