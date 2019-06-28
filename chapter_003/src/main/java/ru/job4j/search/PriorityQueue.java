9552c4ac79e5e33db43cd60f7cb542aafb728583
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2019-06-28 17:25:07 +0500

    PhoneDictionary

---
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,14 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    /**
+     * метод должен вставлять в нужную позирцию элемент
+     * Позицию определять по полю приоритет
+     * Для вставки использовать add(int index, E value)
+     * @param task задача
+     */
+}
