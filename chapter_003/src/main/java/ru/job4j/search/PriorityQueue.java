2d1e9579a1140005736509b17e88cbf553b88437
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2019-07-01 21:40:59 +0500

    Priority

--- chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -9,6 +9,39 @@ public class PriorityQueue {
      * метод должен вставлять в нужную позирцию элемент
      * Позицию определять по полю приоритет
      * Для вставки использовать add(int index, E value)
+     *
      * @param task задача
      */
+
+    public void put(Task task) {
+
+        for (int i = 0; i <= tasks.size(); i++) {
+
+
+            if (tasks.size() == 0) {
+                tasks.add(i, task);
+
+
+            }
+            else if (task.getPriorety() < tasks.getFirst().getPriorety()) {
+                tasks.add(i, task);
+
+            }
+            else if (task.getPriorety() > tasks.getFirst().getPriorety() && task.getPriorety() < tasks.getLast().getPriorety()) {
+                tasks.add(i + 1, task);
+                break;
+            }
+
+        }
+
+
+    }
+
+
+    public Task take() {
+        return this.tasks.poll();
+    }
 }
+
+
+
