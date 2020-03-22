24701377fc52620d1c54bc669ecfc342f0e0518c
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2020-03-22 14:21:05 +0500

    Compare of string

--- chapter_003/src/main/java/ru/job4j/usersort/StringCompare.java
+++ chapter_003/src/main/java/ru/job4j/usersort/StringCompare.java
@@ -6,13 +6,10 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int result = 0;
-        char[] left_char = left.toCharArray();
-        char[] right_char = right.toCharArray();
-        result = Integer.compare(left.length(), right.length());
+        int  result = Integer.compare(left.length(), right.length());
         if (result == 0) {
-            for (int i = 0; i < left_char.length; i++) {
-                result = Character.compare(left_char[i], right_char[i]);
+            for (int i = 0; i < left.length(); i++) {
+                result = Character.compare(left.charAt(i), right.charAt(i) );
                 if (result != 0) {
                     break;
                 }
