2bedb3376ef70b35af8d34adac1204440ff47f62
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2020-06-28 18:20:52 +0500

    Compare Strings

--- chapter_003/src/main/java/ru/job4j/collection/FreezeStr.java
+++ chapter_003/src/main/java/ru/job4j/collection/FreezeStr.java
@@ -1,35 +1,63 @@
 package ru.job4j.collection;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
-
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FreezeStr {
 
     /**
      * Метод сравнивает две строки и определяет явлется ли одна строка переставлением символов другой строки
-     * @param left первая строка
+     *
+     * @param left  первая строка
      * @param right вторая строка
      * @return result возвращает результат
      */
 
     public static boolean eq(String left, String right) {
-        boolean result = false;
-        ArrayList<Character> world1 = new ArrayList<>();
-        ArrayList<Character> world2 = new ArrayList<>();
+        boolean result = true;
+        int key1 = 1;
+        int key2 = 1;
+        int key3 = 0;
+        int counter = 0;
+        int counter1 = 0;
+
+        Map<Integer, Character> world1 = new HashMap<>();
+        Map<Integer, Character> world2 = new HashMap<>();
         for (int i = 0; i < left.length(); i++) {
-            world1.add(left.charAt(i));
+            world1.put(key1, left.charAt(i));
+            key1++;
         }
         for (int j = 0; j < right.length(); j++) {
-            world2.add(right.charAt(j));
+            world2.put(key2, right.charAt(j));
+            key2++;
         }
-        Collections.sort(world1);
-        Collections.sort(world2);
-        if (world1.equals(world2)) {
-            result = true;
+        for (Map.Entry<Integer, Character> entry : world1.entrySet()) {
+            for (Map.Entry<Integer, Character> entry1 : world1.entrySet()) {
+                if (entry.getValue().equals(entry1.getValue())) {
+                    counter++;
+                }
+            }
+            key3 = 0;
+            if (key3 < world2.size()) {
+                for (Map.Entry<Integer, Character> entry4 : world2.entrySet()) {
+                    if (world2.containsValue(entry.getValue())) {
+                        if (entry.getValue().equals(entry4.getValue())) {
+                            counter1++;
+                        }
+                    }
+                    key3++;
+                }
+            }
+                if (counter != counter1) {
+                    result = false;
+                }
+
         }
         return result;
-
     }
 }
+
---
+++ chapter_004/src/main/java/lambda/Fi.java
@@ -0,0 +1,10 @@
+package lambda;
+
+import java.util.Comparator;
+
+public class Fi  {
+    public static void main(String[] args) {
+        Comparator<String> str1 = (left, right) -> left.compareTo(right);
+        
+    }
+}
