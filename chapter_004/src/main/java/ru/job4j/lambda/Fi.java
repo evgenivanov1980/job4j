ca802b2961db37c6e7cb74bea9c99f276c36e1ca
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2020-07-01 19:50:34 +0500

    link to methods

--- chapter_004/src/main/java/ru/job4j/lambda/Fi.java
+++ chapter_004/src/main/java/ru/job4j/lambda/Fi.java
@@ -1,10 +1,14 @@
 package ru.job4j.lambda;
 
 import java.util.Comparator;
+import java.util.TreeSet;
 
 public class Fi  {
     public static void main(String[] args) {
-        Comparator<String> str1 = (left, right) -> left.compareTo(right);
+        Comparator<String> cmpText = (left, right) -> left.compareTo(right);
+        Comparator<String> cmpDescSize = (left, right) -> Integer.compare(left.length(), right.length());
+
+
         
     }
 }
