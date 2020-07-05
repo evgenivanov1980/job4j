ceeca047313613edf704a92da97e28ff2a1c2e37
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2020-07-05 18:28:58 +0500

    PhoneDictionary

--- chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -20,8 +20,11 @@ public class PhoneDictionary {
      */
 
     public List<Person> find(String key) {
-        Predicate<Person> combine = person -> person.getName().contains(key) || person.getSurname().contains(key)
-                || person.getAddress().contains(key) || person.getPhone().contains(key);
+        Predicate<Person> personPredicate = person -> person.getName().contains(key);
+        Predicate<Person> personPredicate1 = person -> person.getSurname().contains(key);
+        Predicate<Person> personPredicate2 = person -> person.getPhone().contains(key);
+        Predicate<Person> personPredicate3 = person -> person.getAddress().contains(key);
+        Predicate<Person> combine = personPredicate.or(personPredicate1).or(personPredicate2).or(personPredicate3);
         List<Person> result = new ArrayList<>();
         for (Person person  : persons) {
             if (combine.test(person)) {
