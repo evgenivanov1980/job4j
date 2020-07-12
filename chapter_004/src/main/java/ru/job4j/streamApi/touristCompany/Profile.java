13fc641393ababb30274637f74a810856a5f43e7
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2020-07-12 18:01:43 +0500

    Tourists's Addresses List

--- chapter_004/src/main/java/ru/job4j/streamApi/touristCompany/Profile.java
+++ chapter_004/src/main/java/ru/job4j/streamApi/touristCompany/Profile.java
@@ -3,7 +3,7 @@ package ru.job4j.streamApi.touristCompany;
 public class Profile {
     private Address address;
 
-    public Address getAddress() {
+    public  Address getAddress() {
         return address;
     }
 }
--- chapter_004/src/main/java/ru/job4j/streamApi/touristCompany/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/streamApi/touristCompany/Profiles.java
@@ -5,17 +5,11 @@ import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
-        List<Address> result;
-        result =  profiles.stream().map( x -> x.getAddress()).collect(Collectors.toList());
-        return result.stream().sorted(new CityComparatot()).distinct().collect(Collectors.toList());
-
-
+        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
     }
-}
-class CityComparatot implements Comparator<Address> {
 
-    @Override
-    public int compare(Address o1, Address o2) {
-        return o1.getCity().compareTo(o2.getCity());
+    public List<Address> collectWithoutDublicate(List<Profile> profiles) {
+        return profiles.stream().map(Profile::getAddress).sorted(Comparator.comparing(Address::getCity)).distinct().collect(Collectors.toList());
+
     }
 }
