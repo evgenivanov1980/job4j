064c43d2204361ce1e95c4c085050291b1f9bda3
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2020-07-12 10:54:59 +0500

    Tourists's Addresses List

--- chapter_004/src/main/java/ru/job4j/streamApi/touristCompany/Address.java
+++ chapter_004/src/main/java/ru/job4j/streamApi/touristCompany/Address.java
@@ -1,8 +1,28 @@
 package ru.job4j.streamApi.touristCompany;
 
+import java.util.Objects;
+
 public class Address {
     private String city;
     private String street;
     private int home;
     private int apartment;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return city.equals(address.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city);
+    }
+
+    public String getCity() {
+        return city;
+    }
 }
+
--- chapter_004/src/main/java/ru/job4j/streamApi/touristCompany/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/streamApi/touristCompany/Profiles.java
@@ -1,11 +1,21 @@
 package ru.job4j.streamApi.touristCompany;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map( x -> x.getAddress()).collect(Collectors.toList());
+        List<Address> result;
+        result =  profiles.stream().map( x -> x.getAddress()).collect(Collectors.toList());
+        return result.stream().sorted(new CityComparatot()).distinct().collect(Collectors.toList());
 
+
+    }
+}
+class CityComparatot implements Comparator<Address> {
+
+    @Override
+    public int compare(Address o1, Address o2) {
+        return o1.getCity().compareTo(o2.getCity());
     }
 }
