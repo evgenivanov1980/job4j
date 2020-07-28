dc4bbac121e1210009c784d1773fbeea1dc6d4fc
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2020-07-28 18:07:04 +0500

    ConvertListToMap

--- chapter_004/src/main/java/ru/job4j/streamApi/students/Roster.java
+++ chapter_004/src/main/java/ru/job4j/streamApi/students/Roster.java
@@ -2,12 +2,12 @@ package ru.job4j.streamApi.students;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Roster {
-    private Student student;
 
     public Map<String, Student> collect(List<Student> students) {
-        return students.stream().distinct().collect(Collectors.toMap(Student::getSurname, student1 -> student));
+        return students.stream().collect(Collectors.toMap(Student::getSurname, Function.identity(), (student, student2) -> student));
     }
 }
--- chapter_004/src/test/java/ru/job4j/streamApi/students/RosterTest.java
+++ chapter_004/src/test/java/ru/job4j/streamApi/students/RosterTest.java
@@ -2,10 +2,8 @@ package ru.job4j.streamApi.students;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
 import static org.hamcrest.core.Is.is;
 
 import static org.junit.Assert.*;
@@ -14,14 +12,17 @@ public class RosterTest {
     @Test
     public void whenStudentIsListThenConvertItToMap() {
         Roster roster = new Roster();
-        Map<String, Student> studentMap = new HashMap<>();
-        List<Student> students = new ArrayList<>();
-        Student one = new Student(5, "Ivanov");
-        Student two = new Student(10, "Petrov");
-        students.add(one);
-        students.add(two);
-        studentMap = roster.collect(students);
-        assertThat(studentMap.get(one.getSurname()), is(one));
+        Map<String, Student> studentMap;
+        List<Student> students = Arrays.asList(
+                new Student(5, "Ivanov"),
+                new Student(10, "Petrov"),
+                new Student(7, "Ivanov")
+        );
+        Map<String, Student> expected = new HashMap<>();
+        expected.put("Ivanov", new Student(5, "Ivanov"));
+        expected.put("Petrov", new Student(10, "Petrov"));
+        Map<String, Student> result = roster.collect(students);
+        assertThat(result, is(expected));
     }
 
 }
