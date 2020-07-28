87c126b19932655240012381381da3c473c64e1c
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2020-07-28 19:06:33 +0500

    ConvertListToMap

--- chapter_004/src/main/java/ru/job4j/streamApi/students/Roster.java
+++ chapter_004/src/main/java/ru/job4j/streamApi/students/Roster.java
@@ -8,6 +8,6 @@ import java.util.stream.Collectors;
 public class Roster {
 
     public Map<String, Student> collect(List<Student> students) {
-        return students.stream().collect(Collectors.toMap(Student::getSurname, Function.identity(), (student, student2) -> student));
+        return students.stream().collect(Collectors.toMap(Student::getSurname, student -> student, (student, student2) -> student));
     }
 }
