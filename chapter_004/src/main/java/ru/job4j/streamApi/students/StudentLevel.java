a52fc17a185776f6988d3f7539553131292752c8
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2020-08-12 22:57:15 +0500

    Student's score

---
+++ chapter_004/src/main/java/ru/job4j/streamApi/students/StudentLevel.java
@@ -0,0 +1,14 @@
+package ru.job4j.streamApi.students;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StudentLevel {
+    public static List<Student> levelof(List<Student> students, int bound) {
+        return students.stream().filter(Objects::nonNull).sorted(Comparator.comparingInt(Student::getScore)).
+                takeWhile(student -> student.getScore() > bound).collect(Collectors.toList());
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/streamApi/students/StudentLevelTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.streamApi.students;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+public class StudentLevelTest {
+    @Test
+    public void whenSorted() {
+        List<Student> input = new ArrayList<>();
+        input.add(new Student(128, "Petrov"));
+        input.add(new Student(28, "Ivanova"));
+        List<Student> expected = List.of(new Student(28, "Ivanova"),
+                                         new Student(128, "Petrov"));
+        assertThat(StudentLevel.levelof(input, 20), is(expected));
+    }
+    @Test
+    public void whenOnlyNull() {
+        List<Student> input = new ArrayList<>();
+        input.add(null);
+        List<Student> expected = List.of();
+        assertThat(StudentLevel.levelof(input, 100), is(expected));
+    }
+    @Test
+    public void whenHasNull() {
+        List<Student> input = new ArrayList<>();
+        input.add(null);
+        input.add(new Student(28, "Petrov"));
+        List<Student> expected = List.of(new Student(28, "Petrov"));
+        assertThat(StudentLevel.levelof(input, 10), is(expected));
+    }
+    @Test
+    public void whenBoundMoreThanTen() {
+        List<Student> input = new ArrayList<>();
+        input.add(new Student(128, "Ivanov"));
+        input.add(new Student(5, "Petrov"));
+        input.add(new Student(28, "Godorenko"));
+        input.add(new Student(7, "Sidorova"));
+        List<Student> expected = List.of(new Student(28, "Godorenko"),
+                                         new Student(128, "Ivanov"));
+        assertThat(StudentLevel.levelof(input, 10), is(expected));
+
+
+    }
+
+}
