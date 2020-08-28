7eb9971cebb8b2e80abe2a1d1f15bb039c17def4
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2020-08-28 21:47:54 +0500

    Analyze pupils

--- chapter_004/src/main/java/ru/job4j/streamapi/pupil/Analyze.java
+++ chapter_004/src/main/java/ru/job4j/streamapi/pupil/Analyze.java
@@ -1,6 +1,8 @@
 package ru.job4j.streamapi.pupil;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Analyze {
@@ -9,4 +11,7 @@ public class Analyze {
                 .flatMap(Collection::stream)
                 .mapToDouble(Subject::getScore).average().orElse(0);
     }
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> pupilStream) {
+        return pupilStream.map(Pupil::getName).map(Pupil::getSubject).flatMap(Collection::stream).flatMap()
+                .mapToDouble(Subject::getScore).average().collect(Collectors.toList());
 }
--- chapter_004/src/test/java/ru/job4j/streamapi/pupil/AnalyzeTest.java
+++ chapter_004/src/test/java/ru/job4j/streamapi/pupil/AnalyzeTest.java
@@ -1,7 +1,32 @@
 package ru.job4j.streamapi.pupil;
 
+import org.junit.Test;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
 
 public class AnalyzeTest {
+    @Test
+    public void whenSinglePupil() {
+        double average = Analyze.averageScore(
+                List.of(
+                        new Pupil("Ivanov", List.of(new Subject("Math", 100)))
+
+                ).stream()
+        );
+        assertThat(average, is(100D));
+    }
+    @Test
+    public void whenPupilAverage() {
+        double average = Analyze.averageScore(
+                List.of(
+                        new Pupil("Ivanov", List.of(new Subject("Math", 100))),
+                        new Pupil("Petrov", List.of(new Subject("Math", 60)))
+                ).stream()
+        );
+        assertThat(average, is(80D));
+    }
 
 }
