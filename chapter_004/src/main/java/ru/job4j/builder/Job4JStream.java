377b6406f637675e88ddd2fa06b88620c9e492e8
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2020-09-25 22:36:51 +0500

    Builder

--- chapter_004/src/main/java/ru/job4j/builder/Job4JStream.java
+++ chapter_004/src/main/java/ru/job4j/builder/Job4JStream.java
@@ -6,16 +6,42 @@ import java.util.function.Predicate;
 public class Job4JStream {
     private List<String> source;
     private Predicate<String> filter;
-    private String[] array;
+
+
+    public static Job4JStream of(List<String> list) {
+        return new Builder()
+                .of(list)
+                .build();
+
+    }
+
+    public static Job4JStream filter(Predicate<String> predicate) {
+        return new Builder()
+                .filter(predicate)
+                .build();
+    }
+
+    public  List<String> collect(List<String> list) {
+        Job4JStream job4j = new Job4JStream();
+        return new Builder()
+                .of(job4j.source)
+                .filter(job4j.filter)
+                .collect(list.forEach())
+                .build();
+    }
+
+
+
+
 
 
     public static class Builder {
-        private String[] array;
+
         private Predicate<String> predicate;
         private List<String> list;
 
-        public Builder of(String[] array) {
-            this.array = array;
+        public Builder of(List<String> list) {
+            this.list = list;
             return self();
         }
 
@@ -29,15 +55,19 @@ public class Job4JStream {
             return self();
         }
 
+        public Job4JStream build() {
+            Job4JStream job4j = new Job4JStream();
+            job4j.source = list;
+            job4j.filter = predicate;
+            return job4j;
+        }
+
         public Builder self() {
             return this;
         }
 
     }
-    public static void myStream() {
-        Job4JStream job4JStream = new Builder().of(array).filter(predicate).collect(list).self();
 
-    }
 
 
 
